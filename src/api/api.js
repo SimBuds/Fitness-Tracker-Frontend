d429cb309dee2954f342dab351c938c4b6e9c8d2
SimBuds <casey-hsu@outlook.com>
2024-06-06 08:01:18 -0400

    Updates To Screens v2

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_URL = 'http://localhost:5000/api';
 
 const api = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: API_URL,
   headers: {
     'Cache-Control': 'no-store',
   },
